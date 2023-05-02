ac5dced459014d25e863a4c6320b6907aaf8cfa5
hacknus <linus.stoeckli@unibe.ch>
2023-05-02 20:18:13 +0200

    initial structure for python package

--- interface.py
+++ interface.py
@@ -1,45 +1,25 @@
+import time
+
 import numpy as np
 import platform
 from scipy.fft import rfft, rfftfreq
 import socket
 import subprocess  # For executing a shell command
 import logging
+import queue
 
 
-class TopticaConfigSocket:
-    def __init__(self, dataclass, debug):
-        self.r_dat_header = b'\xcd\xef\x124x\x9a\xfe\xdc\x00\x00\x00\x01\x00\xff\x91\xe7\x03\xe8\x00\x00'
+class TopticaSocket:
+    def __init__(self, ip, data):
         self.send_header = b'\xcd\xef\x124x\x9a\xfe\xdc\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00'
         self.r_stat_header = b'\xcd\xef\x124x\x9a\xfe\xdc\x00\x00\x00\x03\x00\x00\x00\x00\x00\x00\x02'
-        self.r_dat_header = b'\xcd\xef\x124x\x9a\xfe\xdc\x00\x00\x00\x01\x00\xff\x91\xe7\x03\xe8\x00'
-        self._dataclass = dataclass
-        self._dataclass.time = np.linspace(2070, 2070 + 50, 1000)
-
-        self.setup_cmds = [
-            (b'\x14', "SYSTEM : TELL STATUS"),
-            (b'\x0b', "CHANNEL : D"),
-            (b'\x0f', "MOTION : NORMAL"),
-            (b'\x16', "TRANSMISSION : SLIDING"),
-            (b'\x11', "SYSTEM : TIA ATN2"),
-            # (b'\x1a', "ACQUISITION : BEGIN 1070.0"),
-            (b'\x1a', "ACQUISITION : BEGIN 1000.0"),
-            (b'\x17', "ACQUISITION : AVERAGE 2"),
-            (b'\x12', "ACQUISITION : STOP"),
-            (b'\x18', "ACQUISITION : RANGE 0.00"),
-            (b'\x17', "ACQUISITION : RESET AVG"),
-            (b'\x19', "ACQUISITION : RANGE 50.00"),
-            (b'\x12', "SYSTEM : MONITOR 1"),
-            (b'\x0a', "LASER : ON"),
-            (b'\x0a', "VOLT1 : ON"),
-            (b'\x13', "ACQUISITION : START"),
-        ]
-
-        if debug:
-            ip = "localhost"
-        else:
-            ip = "169.254.84.101"
-        self.config_server_adress = (ip, 6341)
+        self.r_dat_header = b'\xcd\xef\x124x\x9a\xfe\xdc\x00\x00\x00\x01\x00'
+        self.config_server_address = (ip, 6341)
+        self.data_server_address = (ip, 6342)
+        self.range = 50
         self.running = True
+        self.status = ""
+        self._data = data
 
     def wait_for_answer(self, client, length=1024):
         while self.running:
@@ -50,44 +30,43 @@ class TopticaConfigSocket:
                 elif "MON" in data.decode("utf-8", "ignore"):
                     return 1
             else:
-                logging.error("Client termianted with FIN.")
+                logging.error("[TCP CONF] Client terminated with FIN.")
                 return 0
 
-    def run(self):
+    def run_conf_tcp(self, cmd_queue):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(self.config_server_adress)
-            logging.info(f"Starting server at address {self.config_server_adress}")
+            s.bind(self.config_server_address)
+            logging.info(f"[TCP CONF] Starting server at address {self.config_server_address}")
             s.listen()
             client, addr = s.accept()
-            logging.info(f"Connected by client with address {addr}")
-            for cmd in self.setup_cmds:
-                b, c = cmd
-                message = self.send_header + b + c.encode()
-                client.send(message)
-                if self.wait_for_answer(client) == 0:
-                    return
+            logging.info(f"[TCP CONF] Connected by client with address {addr}")
+
             while self.running:
-                b, c = (b'\x12', "SYSTEM : MONITOR 1")
-                message = self.send_header + b + c.encode()
-                client.send(message)
-                if self.wait_for_answer(client) == 0:
-                    return
 
-
-class TopticaDataSocket:
-    def __init__(self, dataclass, debug):
-        self.r_dat_header = b'\xcd\xef\x124x\x9a\xfe\xdc\x00\x00\x00\x01\x00\xff\x91\xe7\x03\xe8\x00'
-        self.r_dat_header = b'\xcd\xef\x124x\x9a\xfe\xdc\x00\x00\x00\x01\x00'
-        self._dataclass = dataclass
-        self.running = True
-        if debug:
-            ip = "localhost"
-        else:
-            ip = "169.254.84.101"
-
-        self.data_server_adress = (ip, 6342)
-        if not self.ping(ip):
-            raise Exception("[ERROR] ping timeout! Check LAN connection.")
+                # check the queue for commands
+                cmd = cmd_queue.get()
+                if cmd:
+                    (b, s) = cmd
+                    message = self.send_header + b + s.encode()
+                    client.send(message)
+                    if b == b'0x14':
+                        # if we request the status, save the response
+                        while self.running:
+                            self.status = client.recv(1024).decode("utf-8", "ignore")
+                        print(self.status)
+                    elif "RANGE" in s:
+                        self.range = float(s[-6:])
+                    else:
+                        if self.wait_for_answer(client) == 0:
+                            return
+                else:
+                    # just to a simple heartbeat
+                    b, c = (b'\x12', "SYSTEM : MONITOR 1")
+                    message = self.send_header + b + s.encode()
+                    client.send(message)
+                    if self.wait_for_answer(client) == 0:
+                        return
+                time.sleep(0.25)
 
     def ping(self, host):
         res = False
@@ -99,21 +78,28 @@ class TopticaDataSocket:
             res = True
         return res
 
-    def read(self):
+    def run_tcp_dat(self):
         types = np.dtype([
             ("signal_1", np.int16),
-            ("ref_1", np.int16),
+            ("reserved_1", np.int16),
             ("signal_2", np.int16),
-            ("ref_2", np.int16),
+            ("reserved_2", np.int16),
         ])
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(self.data_server_adress)
-            logging.info(f"Starting server at address {self.data_server_adress}")
+            s.bind(self.data_server_address)
+            logging.info(f"[TCP DAT] Starting server at address {self.data_server_address}")
             s.listen()
             client, addr = s.accept()
-            logging.info(f"Connected by client with address {addr}")
+            logging.info(f"[TCP DAT] Connected by client with address {addr}")
+            old_range = self.range
             while self.running:
-                raw_data = client.recv(8060)
+                if old_range != self.range:
+                    # range has changed and needs to be adjusted
+                    pass
+
+                old_range = self.range
+
+                raw_data = client.recv(2 * 4 * (20 * self.range + 1) + 52)
 
                 if not raw_data:
                     continue
@@ -126,26 +112,24 @@ class TopticaDataSocket:
                     except IndexError:
                         continue
                 while True:
-                    if len(data) != 8000:
-                        data = data + client.recv(8000 - len(data))
+                    if len(data) != 2 * 4 * (20 * self.range + 1):
+                        data = data + client.recv(2 * 4 * (20 * self.range + 1) - len(data))
                     else:
                         break
                 try:
                     types = types.newbyteorder('>')
                     arr = np.frombuffer(data, dtype=types)
-                    self._dataclass.signal_1 = (arr['signal_1'] - arr['signal_1'][0]) / 20
-                    self._dataclass.ref_1 = arr['ref_1']
-                    self._dataclass.signal_2 = (arr['signal_2'] - arr['signal_2'][0]) / 20
-                    self._dataclass.ref_2 = arr['ref_2']
+                    self._data.signal_1 = arr['signal_1'] / 20.0 - arr['signal_1'][0] / 20.0
+                    self._data.signal_2 = arr['signal_2'] / 20.0 - arr['signal_2'][0] / 20.0
 
-                    t = self._dataclass.time
-                    p = self._dataclass.signal_1
-                    SAMPLE_RATE = len(t) / (t[-1] - t[0]) * 1e12
-                    N = len(p)
+                    t = self._data.time
+                    p = self._data.signal_1
+                    sample_rate = len(t) / (t[-1] - t[0]) * 1e12
+                    n = len(p)
                     a = rfft(p)
-                    f = rfftfreq(N, 1 / SAMPLE_RATE)
-                    self._dataclass.freq_1 = f / 1e12
-                    self._dataclass.freq_1_amp = np.abs(a)
+                    f = rfftfreq(n, 1 / sample_rate)
+                    self._data.freq_1 = f / 1e12
+                    self._data.freq_1_amp = np.abs(a)
                 except Exception as e:
                     logging.error(e)
                     logging.error(f"{len(data)=}")
---
+++ teraflash.py
@@ -0,0 +1,182 @@
+import logging
+import queue
+import threading
+import time
+
+import numpy as np
+
+from interface import TopticaSocket
+
+
+class DataContainer:
+
+    def __init__(self, n=1000):
+        self.time = np.zeros(n)
+
+        self.signal_1 = np.zeros(n)
+        self.freq_1 = np.zeros(n)
+        self.freq_1_amp = np.zeros(n)
+        self.freq_1_phase = np.zeros(n)
+
+        self.signal_2 = np.zeros(n)
+        self.freq_2 = np.zeros(n)
+        self.freq_2_amp = np.zeros(n)
+        self.freq_2_phase = np.zeros(n)
+
+
+class TeraFlash:
+
+    def __init__(self, ip="169.254.84.101"):
+        self.r_dat_header = b'\xcd\xef\x124x\x9a\xfe\xdc\x00\x00\x00\x01\x00\xff\x91\xe7\x03\xe8\x00\x00'
+        self.send_header = b'\xcd\xef\x124x\x9a\xfe\xdc\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00'
+        self.r_stat_header = b'\xcd\xef\x124x\x9a\xfe\xdc\x00\x00\x00\x03\x00\x00\x00\x00\x00\x00\x02'
+        self.r_dat_header = b'\xcd\xef\x124x\x9a\xfe\xdc\x00\x00\x00\x01\x00\xff\x91\xe7\x03\xe8\x00'
+
+        logging.basicConfig(filename=f"logs/teraflash_{int(time.time())}.log", level=logging.DEBUG)
+        logging.getLogger().addHandler(logging.StreamHandler())
+
+        self.ip = ip
+        self.cmd_queue = queue.Queue()
+        self.data = DataContainer()
+
+        socket = TopticaSocket(self.ip, self.data)
+
+        # launch tcp config socket
+        config_thread = threading.Thread(target=socket.run_conf_tcp, args=(self.cmd_queue,))
+
+        # launch tcp data socket
+        data_thread = threading.Thread(target=socket.run_conf_tcp)
+
+        data_thread.start()
+        config_thread.start()
+
+    def setup(self):
+        logging.info("[CMD] setting up the device...")
+        self.get_sys_status()
+        self.get_sys_status()
+        self.set_channel()
+        self.set_mode()
+        self.set_transmission()
+        self.set_antenna()
+        self.set_acq_begin()
+        self.set_acq_avg()
+        self.set_acq_stop()
+        self.set_acq_range()
+        self.get_sys_monitor()
+        self.set_acq_avg()
+        self.set_acq_range()
+        self.get_sys_monitor()
+        self.get_sys_status()
+
+    def get_sys_status(self):
+        logging.debug("[CMD] requesting status")
+        cmd = (b'\x14', "SYSTEM : TELL STATUS")
+        self.cmd_queue.put(cmd)
+
+    def get_sys_monitor(self):
+        cmd = (b'\x12', "SYSTEM : MONITOR 1")
+        self.cmd_queue.put(cmd)
+
+    def set_channel(self, channel="D"):
+        logging.debug(f"[CMD] setting channel: {channel}")
+        cmd = (b'\x0b', f"CHANNEL : {channel}")
+        self.cmd_queue.put(cmd)
+
+    def set_mode(self, motion="NORMAL"):
+        logging.debug(f"[CMD] setting motion: {motion}")
+        cmd = (b'\x0f', f"CHANNEL : {motion}")
+        self.cmd_queue.put(cmd)
+
+    def set_transmission(self, transmission="SLIDING"):
+        logging.debug(f"[CMD] setting transmission: {transmission}")
+        cmd = (b'\x16', f"TRANSMISSION : {transmission}")
+        self.cmd_queue.put(cmd)
+
+    def set_antenna(self):
+        logging.debug("[CMD] setting antenna: TIA ATN2")
+        cmd = (b'\x11', "SYSTEM : TIA ATN2")
+        self.cmd_queue.put(cmd)
+
+    def set_acq_begin(self, t_begin=1000.0):
+        logging.debug(f"[CMD] setting acq begin: {t_begin}")
+        if t_begin < 10:
+            b = b'0x18'
+        elif t_begin < 100:
+            b = b'0x19'
+        else:
+            b = b'0x1a'
+        cmd = (b, f"ACQUISITION : BEGIN {t_begin:.1f}")
+        self.cmd_queue.put(cmd)
+
+    @staticmethod
+    def nearest_entry(t_range, available_ranges):
+        nearest = available_ranges[0]
+        for num in available_ranges:
+            if abs(t_range - num) < abs(t_range - nearest):
+                nearest = num
+        return nearest
+
+    def set_acq_range(self, t_range=50.0):
+        available_ranges = [5, 10, 15, 20, 35, 50, 70, 100, 120, 150, 200]
+        if t_range not in available_ranges:
+            logging.info(f"[CMD] {t_range} is not supported. Only {available_ranges} are supported.")
+            t_range = self.nearest_entry(t_range, available_ranges)
+        logging.debug(f"[CMD] setting acq range: {t_range}")
+        if t_range <= 5.0:
+            b = b'0x18'
+        elif t_range <= 70.0:
+            b = b'0x19'
+        else:
+            b = b'0x1a'
+        cmd = (b, f"ACQUISITION : RANGE {t_range:.2f}")
+        self.cmd_queue.put(cmd)
+
+    def set_acq_avg(self, avg=2):
+        logging.debug(f"[CMD] setting acq avg: {avg}")
+        if avg < 10:
+            b = b'0x17'
+        elif avg < 100:
+            b = b'0x18'
+        elif avg < 1000:
+            b = b'0x19'
+        else:
+            b = b'0x1a'
+        cmd = (b, f"ACQUISITION : AVERAGE {avg}")
+        self.cmd_queue.put(cmd)
+
+    def reset_acq_avg(self):
+        logging.debug("[CMD] resetting acq avg")
+
+        cmd = (b'0x17', "ACQUISITION : RESET AVG")
+        self.cmd_queue.put(cmd)
+
+    def set_laser(self, state):
+        if state:
+            logging.debug("[CMD] setting laser on")
+            cmd = (b'0x0a', "LASER : ON")
+        else:
+            logging.debug("[CMD] setting laser off")
+            cmd = (b'0x0b', "LASER : OFF")
+        self.cmd_queue.put(cmd)
+
+    def set_emitter(self, emitter, state):
+        if emitter not in [1, 2]:
+            logging.info(f"emitter {emitter} is invalid, please use 1 or 2 as emitter value")
+            return
+        if state:
+            logging.debug(f"[CMD] setting emitter {emitter} on")
+            cmd = (b'0x0a', f"VOLT{emitter} : ON")
+        else:
+            logging.debug(f"[CMD] setting emitter {emitter} off")
+            cmd = (b'0x0b', f"VOLT{emitter} : OFF")
+        self.cmd_queue.put(cmd)
+
+    def set_acq_start(self):
+        logging.debug("[CMD] starting acquisition")
+        cmd = (b'0x13', "ACQUISITION : START")
+        self.cmd_queue.put(cmd)
+
+    def set_acq_stop(self):
+        logging.debug("[CMD] stopping acquisition")
+        cmd = (b'0x12', "ACQUISITION : STOP")
+        self.cmd_queue.put(cmd)
