25dc4e061da289bebe6eb530e5668347b4392ebb
hacknus <linus.stoeckli@unibe.ch>
2023-05-05 14:30:11 +0200

    plot data

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import time
+
 from teraflash import TeraFlash
+import matplotlib.pyplot as plt
+
 
 if __name__ == "__main__":
     with TeraFlash() as device:
         print(f"status: {device.get_status()}")
+
+        device.set_laser(True)
+        device.set_emitter(1, True)
+        device.set_acq_start()
+        # wait some time to gather data
+        time.sleep(1)
         print(device.get_data().signal_1)
+
+    plt.plot(device.get_data().time, device.get_data().signal_1)
+    plt.show()
+
