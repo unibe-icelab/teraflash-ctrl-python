af569fca088f229205213ec7a87562d92bf63e9d
hacknus <linus.stoeckli@unibe.ch>
2026-02-26 17:22:57 +0100

    change copyright

--- src/math_utils.py
+++ src/math_utils.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2026 Linus Leo Stöckli
+Copyright (c) 2026 University of Bern, Space Research & Planetary Sciences, Linus Leo Stöckli.
 
 This work is licensed under the Creative Commons
 Attribution-NonCommercial 4.0 International License.
