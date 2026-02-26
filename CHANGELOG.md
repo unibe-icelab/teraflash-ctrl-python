71230fb74e757482dd95ce009d19fcbed7ecaf1a
hacknus <linus.stoeckli@unibe.ch>
2026-02-26 17:09:37 +0100

    license under CC BY NC 4.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,9 +2,13 @@
 
 All notable changes to the `teraflash-ctrl-python` project will be documented in this file.
 
-# Unreleased 1.3.x
+# Unreleased 1.4.x
 
 * ...
+
+# 1.4.0
+
+* migrate to GitHub, add License
 
 # 1.3.0
 
--- src/interface.py
+++ src/interface.py
@@ -1,3 +1,12 @@
+"""
+Copyright (c) 2026 Linus Leo Stöckli
+
+This work is licensed under the Creative Commons
+Attribution-NonCommercial 4.0 International License.
+To view a copy of this license, visit
+https://creativecommons.org/licenses/by-nc/4.0/
+"""
+
 import time
 from queue import Queue, Empty
 import threading
